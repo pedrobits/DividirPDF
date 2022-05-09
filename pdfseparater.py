3dbaebb0ed3236586914157c94a799d76f50be43
pedrobits <pedroksk2@gmail.com>
2022-05-09 16:09:27 -0300

    Separador de PDF

---
+++ pdfseparater.py
@@ -0,0 +1,44 @@
+from asyncio import sleep
+from multiprocessing.connection import wait
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import sys
+import msvcrt
+from unicodedata import normalize
+from time import sleep
+
+#Criando uma função com a biblioteca msvcrt para "press any key for continue"
+def wait():
+    msvcrt.getch()
+        
+#Entrada do arquivo PDF que será divido
+inputpdf = PdfFileReader(open("finaz.pdf", "rb"))
+
+#Criando uma função com a biblioteca PyPDF2 para dividir as folhas do PDF
+def separar():
+    for i in range(inputpdf.numPages):
+        output = PdfFileWriter()
+        output.addPage(inputpdf.getPage(i))
+        with open("largefile-page%s.pdf" % i, "wb") as outputStream:
+            output.write(outputStream)
+
+#Var guardando a quantidade de paginas
+numero_de_paginas = inputpdf.numPages
+
+#Input com a decisão de NÃO ou SIM para dividir o PDF
+pergunta1 = input("O numero de paginas a serem divididas é de {}. Deseja continuar? (Sim ou Não): ".format(numero_de_paginas))
+resposta = pergunta1.upper()
+
+if resposta == "SIM" or "YES":
+    print("Separando...")
+    sleep(2)
+    separar()
+    print("Foi!")
+elif resposta == "NÃO" or "NAO":
+    print("Obrigado por usar o Agille PDFSPLITER")
+    print("Pressione Qualquer Botão para sair")
+    wait()
+    sys.exit()
+else:
+    print("Comando Invalido")
+    sys.exit()
+
