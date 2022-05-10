f2f0cf23801c0a4b3917efcd602af81d47145cd9
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2022-05-10 16:16:03 -0300

    style: format, readme e var arquivo

---
+++ README.md
@@ -0,0 +1,7 @@
+# DividirPDF
+
+Usando...
+- [**PyPDF2**](https://pypi.org/project/PyPDF2/)
+- [**msvcrt**](https://docs.python.org/3/library/msvcrt.html)
+- [**asyncio**](https://docs.python.org/3/library/asyncio.html)
+- [**sys**](https://docs.python.org/3/library/sys.html?highlight=sys#module-sys), [**time**](https://docs.python.org/3/library/time.html?highlight=time#module-time) 
--- pdfseparater.py
+++ pdfseparater.py
@@ -1,31 +1,35 @@
 from asyncio import sleep
-from multiprocessing.connection import wait
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import sys
 import msvcrt
-from unicodedata import normalize
 from time import sleep
 
-#Criando uma função com a biblioteca msvcrt para "press any key for continue"
+# PDF
+arquivo = "arquivo.pdf"
+
+# Criando uma função com a biblioteca msvcrt para "press any key for continue"
+
 def wait():
     msvcrt.getch()
-        
-#Entrada do arquivo PDF que será divido
-inputpdf = PdfFileReader(open("finaz.pdf", "rb"))
 
-#Criando uma função com a biblioteca PyPDF2 para dividir as folhas do PDF
+# Entrada do arquivo PDF que será divido
+inputpdf = PdfFileReader(open(arquivo, "rb"))
+
+# Criando uma função com a biblioteca PyPDF2 para dividir as folhas do PDF
+
 def separar():
     for i in range(inputpdf.numPages):
         output = PdfFileWriter()
         output.addPage(inputpdf.getPage(i))
-        with open("largefile-page%s.pdf" % i, "wb") as outputStream:
+        with open("pagina%s.pdf" % i, "wb") as outputStream:
             output.write(outputStream)
 
-#Var guardando a quantidade de paginas
+# Var guardando a quantidade de paginas
 numero_de_paginas = inputpdf.numPages
 
-#Input com a decisão de NÃO ou SIM para dividir o PDF
-pergunta1 = input("O numero de paginas a serem divididas é de {}. Deseja continuar? (Sim ou Não): ".format(numero_de_paginas))
+# Input com a decisão de NÃO ou SIM para dividir o PDF
+pergunta1 = input(
+    "O numero de paginas a serem divididas é de {}. Deseja continuar? (Sim ou Não): ".format(numero_de_paginas))
 resposta = pergunta1.upper()
 
 if resposta == "SIM" or "YES":
@@ -41,4 +45,3 @@ elif resposta == "NÃO" or "NAO":
 else:
     print("Comando Invalido")
     sys.exit()
-
